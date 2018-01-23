c9f77829fc40b41a4d74ebd291181308915b00b3
Jacob Cody Wimer <jacob.wimer@gmail.com>
2018-01-23 11:22:10 -0500

    Added docker repo

--- setup/build.sh
+++ setup/build.sh
@@ -5,6 +5,12 @@ COMPOSE_VERSION='1.15.0'
 DOCKER_VERSION='17.06.2'
 
 add-apt-repository ppa:ondrej/php -y
+# Install docker key and repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository -y \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 apt-get update -qq
 apt-get install -y -qq \
 	software-properties-common \
@@ -23,7 +29,6 @@ apt-get install -y -qq \
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # Install docker and compose
-apt-get update
 apt-get install -y -qq docker-ce=${DOCKER_VERSION}~ce-0~ubuntu
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
