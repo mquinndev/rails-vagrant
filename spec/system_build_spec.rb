15772efee77b8ac31f7c85f1fca9857bc26ece0e
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-03-18 16:42:34 -0400

    system_build.sh runs successfully

--- spec/system_build_spec.rb
+++ spec/system_build_spec.rb
@@ -5,3 +5,14 @@ describe 'Canary Test' do
       expect(true).to eq true
   end
 end
+
+describe 'My Script' do
+  subject { create_stubbed_env }
+
+  it 'system_build.sh runs successfully' do
+    stdout, stderr, status = subject.execute(
+      'scripts/system_build.sh'
+    )
+    expect(status.exitstatus).to eq 0
+  end
+end
